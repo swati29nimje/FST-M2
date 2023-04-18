09544765983cb7675ae0c730df4207818aef0847
swati29nimje <125908788+swati29nimje@users.noreply.github.com>
2023-04-18 16:50:24 +0530

    Add files via upload
    simpleweb activity committed.

--- Docker Project/index.js
+++ Docker Project/index.js
@@ -1,27 +1,11 @@
-const express = require("express");
-const redis = require("redis");
+const express = require('express');
+    
+const app = express(); 
+    
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+}); 
 
-const app = express();
-const client = redis.createClient({
-   host: "redis-server",
-   port: 6379
-});
-
-client.get("visits", (err, visits) => {
-  if (visits === null) {
-    client.set("visits", 0);
-  } else {
-    client.set("visits", parseInt(visits));
-  }
-});
-
-app.get("/", (req, res) => {
-  client.get("visits", (err, visits) => {
-    res.send("Number of visits is " + visits);
-    client.set("visits", parseInt(visits) + 1);
-  });
-});
-
-app.listen(8081, () => {
-  console.log("Listening on port 8081");
+app.listen(8080, () => {
+    console.log("Listening on port 8080");
 });
